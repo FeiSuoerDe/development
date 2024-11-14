2b9639fd9ce2f7e30b7c46ed342c0af02621ab32
fi <2153602387@qq.com>
2024-11-14 13:11:57 +0800

    初始化

--- Com/World/World.cs
+++ Com/World/World.cs
@@ -1,6 +1,8 @@
 using Godot;
 using System;
+using System.ComponentModel;
 
+[Tool]
 public partial class World : Node
 {
 	[Export]
@@ -9,15 +11,33 @@ public partial class World : Node
 	public TileMapLayer River;
 	[Export]
 	public TileMapLayer Ecolog;
+	[Export]
+	public FastNoiseLite fastNoiseLite;
+	[Export]
+	public int seed;
+	[Export]
+	public Vector2I MapSize;
+	[Export, Category("Player")]
+	public double TerrDublue;
 
 	public override void _Ready()
 	{
+		redNoise();
+
 		MakeMap();
 	}
-
-
-	public override void _Process(double delta)
+	// 初始化噪声
+	public void redNoise()
 	{
+		fastNoiseLite = new FastNoiseLite();
+
+		fastNoiseLite.SetSeed(seed);
+
+		if (MapSize.X == 0)
+		{
+			MapSize = new Vector2I(500, 500);
+		}
+
 	}
 	private void MakeMap()
 	{
@@ -25,6 +45,15 @@ public partial class World : Node
 	}
 	private void setTerr()
 	{
+		for (int X = 0; X < MapSize.X; X++)
+		{
+			for (int Y = 0; Y < MapSize.Y; Y++)
+			{
+				double noise = fastNoiseLite.GetNoise2D(X, Y);
+
+			}
+
+		}
 
 	}
 }
